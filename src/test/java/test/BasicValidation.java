99842e508b0a032f74eadf4177c5acb504957555
Ramesh A <catchrameshhere@gmai.com>
2022-02-09 17:14:07 +0530

    initial push

---
+++ src/test/java/test/BasicValidation.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class BasicValidation extends BaseTest{
+	@Test
+	public void titleValidation() {
+		String expectedTitle = "Facebook – log in or sign up";
+		String actualTitle = driver.getTitle();
+		
+		Reporter.log("Expected Title = " + expectedTitle);
+		Reporter.log("Actual Title = " + actualTitle);
+		
+		assertTrue(actualTitle.equals(expectedTitle), "Page title validation failed,");
+	}
+	
+	@Test
+	public void urlValidation() {
+		String expectedUrl = "https://www.facebook.com/";
+		String actualUrl = driver.getCurrentUrl();
+		
+		Reporter.log("Expected URL = " + expectedUrl);
+		Reporter.log("Actual URL = " + actualUrl);
+		
+		assertTrue(actualUrl.equals(expectedUrl), "URL validation failed,");
+		
+	}
+}
